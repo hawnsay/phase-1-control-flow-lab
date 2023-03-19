9cef722c719cd3f16565acdc0b43aa5291418e99
Shawn Chestine <shawn@Shawns-MBP.askey.com>
2023-03-18 22:13:43 -0700

    Completes control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance, message){
+  if (distance <= 400) {
+    message = "This one is on me!";
+  } if (distance < 2000 && distance > 400) {
+    message = "That will be twenty bucks.";
+  }
+  if (distance > 2000) {
+    message = "I will gladly take your thirty bucks.";
+  }
+  if (distance > 2500) {
+    message = "No can do.";
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  let message;
+    switch(tip) {
+     case "generous":
+       message = "Thank you so much.";
+       break;
+     case "not as generous":
+       message = "Thank you."
+       break;
+     default:
+       message = "Bye."
+       break;
+    }
+     return message;
+   }
+   
